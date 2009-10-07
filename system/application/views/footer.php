ac041c2fc92a1357216ea3caad23e6fe0523fa14
Stuart Lewis <stuart@stuartlewis.com>
2009-10-07 15:17:18 +1300

    add link to EasyDeposit in footer

--- system/application/views/footer.php
+++ system/application/views/footer.php
@@ -1,5 +1,5 @@
 		<div id="footer">
-			EasyDeposit SWORD client -
+			<a href="http://github.com/stuartlewis/EasyDeposit">EasyDeposit</a> SWORD client -
 			Developed by <a href="http://stuartlewis.com/">Stuart Lewis</a>
 			at the <a href="http://www.library.auckland.ac.nz/">University of Auckland Library</a><br />
 			Page rendered in {elapsed_time} seconds
