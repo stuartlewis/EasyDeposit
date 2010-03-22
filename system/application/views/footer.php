2ef6d5ac004f873ee0b415385700e5dd995c0a74
Stuart Lewis <stuart@stuartlewis.com>
2010-03-22 16:20:24 +1300

    correcting UoA name in footer

--- system/application/views/footer.php
+++ system/application/views/footer.php
@@ -1,7 +1,7 @@
 		<div id="footer">
 			<a href="http://github.com/stuartlewis/EasyDeposit">EasyDeposit</a> SWORD client -
 			Developed by <a href="http://stuartlewis.com/">Stuart Lewis</a>
-			at the <a href="http://www.library.auckland.ac.nz/">University of Auckland Library</a><br />
+			at <a href="http://www.library.auckland.ac.nz/">The University of Auckland Library</a><br />
 			Page rendered in {elapsed_time} seconds
 		</div>
 	</body>
