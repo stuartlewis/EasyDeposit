fe73487f91e9b4d61b9507c18ff58e69601766a1
Stuart Lewis <stuart@stuartlewis.com>
2010-04-04 06:42:36 +1200

    Updated location of Code Igniter user manual and improved welcome page.

--- system/application/views/welcome_message.php
+++ system/application/views/welcome_message.php
@@ -1,14 +1,21 @@
 <p>
-	Welcome to the EasyDeposit client. The web client is extensible and customisable:
+	Welcome to the EasyDeposit client. The web client is extensible and customisable.
 </p>
-<ul>
-	<li>Access the <a href="admin/">administrative interface</a></li>
-    <li>Customise this welcome screen in system/application/views/welcome_message.php</li>
-	<li>Change the style and look and feel in css/style.css</li>
-	<li>Make the header and footer match your corporate look and feel in system/application/views/header.php and footer.php</li>
-	<li>Change the authentication method to match your local requirements</li>
-	<li>Add extra processing steps to collect more information, skip steps, or add in custom actions</li>
-</ul>
+
+<div class="section">
+    <ul>
+        <li>Access the <a href="admin/">administrative interface</a> or edit files to:
+            <ul>
+                <li>Customise this welcome screen in system/application/views/welcome_message.php</li>
+                <li>Change the style and look and feel in css/style.css</li>
+                <li>Make the header and footer match your corporate look and feel in system/application/views/header.php and footer.php</li>
+                <li>Change the authentication method to match your local requirements</li>
+                <li>Add extra processing steps to collect more information, skip steps, or add in custom actions</li>
+            </ul>
+        </li>
+    </ul>
+</div>
+
 <p>
 	The client is written using the <a href="http://codeigniter.com/">CodeIgniter</a> MVC PHP framework
 	(<a href="user_guide/">user guide</a>)
