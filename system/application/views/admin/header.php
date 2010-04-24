00e3d54d25491116167773ba8b0984b65e21571b
Stuart Lewis <stuart@stuartlewis.com>
2010-04-24 15:10:11 +1200

    Fix tab formatting in file

--- system/application/views/admin/header.php
+++ system/application/views/admin/header.php
@@ -1,10 +1,10 @@
 <?php $this->load->helper('url'); ?>
 <html>
-	<head>
+    <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<base href="<?php echo base_url() ?>">
-		<title>EasyDeposit Admin: <?php echo $page_title; ?></title>
-		<link rel='stylesheet' type='text/css' media='all' href='css/adminstyle.css' />
+        <base href="<?php echo base_url() ?>">
+        <title>EasyDeposit Admin: <?php echo $page_title; ?></title>
+        <link rel='stylesheet' type='text/css' media='all' href='css/adminstyle.css' />
         <link rel="SHORTCUT ICON" href="favicon.ico">
         <?php if (!empty($javascript)) { foreach ($javascript as $js): ?>
         <script type="text/javascript" src="js/<?php echo $js; ?>"></script>
@@ -39,6 +39,6 @@
                     }
                 </script>      
         <?php } ?>
-	</head>
-	<body>
-		<div id="header"><h1><?php echo $page_title; ?></h1></div>
+    </head>
+    <body>
+        <div id="header"><h1><?php echo $page_title; ?></h1></div>
