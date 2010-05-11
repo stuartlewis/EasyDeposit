c118e5884736232ca944e02d569abbb6dff3aca9
Stuart Lewis <stuart@stuartlewis.com>
2010-05-12 10:13:21 +1200

    Update to better support servers without mod_rewrite

--- system/application/views/header.php
+++ system/application/views/header.php
@@ -2,12 +2,12 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <base href="<?php echo base_url() ?>">
+        <base href="<?php echo base_url() . index_page(); if (index_page() !== '') { echo '/'; } ?>">
         <title><?php echo $page_title; ?></title>
-        <link rel='stylesheet' type='text/css' media='all' href='css/style.css' />
+        <link rel='stylesheet' type='text/css' media='all' href='<?php echo base_url(); ?>css/style.css' />
         <link rel="SHORTCUT ICON" href="favicon.ico">
         <?php if (!empty($javascript)) { foreach ($javascript as $js): ?>
-        <script type="text/javascript" src="js/<?php echo $js; ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>js/<?php echo $js; ?>"></script>
         <?php endforeach; } ?>
     </head>
     <body>
