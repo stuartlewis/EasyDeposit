934186f46babec00c3d0a3907e56f68e9131a75f
Stuart Lewis <stuart@stuartlewis.com>
2011-02-04 20:15:07 +1300

    Upgrade to codeigniter v2.0

--- system/application/views/defaults/footer.php
+++
@@ -1,7 +0,0 @@
-        <div id="footer">
-        <a href="http://github.com/stuartlewis/EasyDeposit">EasyDeposit</a> SWORD client -
-            Developed by <a href="http://stuartlewis.com/">Stuart Lewis</a>
-            at <a href="http://www.library.auckland.ac.nz/">The University of Auckland Library</a>
-        </div>
-    </body>
-</html>
