0544bb40acc5dfa470e646f50d770e4e0e7340a5
Stuart Lewis <stuart@stuartlewis.com>
2014-09-08 19:40:52 +0100

    Adding GitHub repository deposit step

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,8 +1,7 @@
 		<div id="footer">
             == Administrative Interface ==<br />
 			<a href="http://easydeposit.swordapp.org/">EasyDeposit</a> SWORD client -
-			Developed by <a href="http://stuartlewis.com/">Stuart Lewis</a>
-			at <a href="http://www.library.auckland.ac.nz/">The University of Auckland Library</a><br />
+			Developed by <a href="http://stuartlewis.com/">Stuart Lewis</a><br />
 			Page rendered in {elapsed_time} seconds
 		</div>
 	</body>
